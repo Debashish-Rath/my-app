2f40c977c413731960448c2b1e2288f64aaad85f
Debashish-Rath <debashishrath123@gmail.com>
2023-07-17 10:18:01 +0530

    initial commit

---
+++ appgenrator.py
@@ -0,0 +1,209 @@
+from flask import Flask, render_template, request, send_file, redirect
+import os
+import tempfile
+import shutil
+import zipfile
+from zipfile import ZipFile
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        try:
+            title = request.form['title']
+            variable_input_2 = int(request.form['variable_input_2'])
+
+            n_values = []
+            for i in range(1, variable_input_2+1):
+                input_name = 'input_' + str(i)
+                value = request.form.get(input_name)
+                n_values.append(value)
+            print(n_values)
+            open_id = request.form['open_id']
+            prompt = request.form['prompt']
+
+            # Define the new project's directory
+
+            temp_dir = tempfile.gettempdir()
+
+            # project_directory = os.path.join(app.root_path, title)
+            project_directory = os.path.join(temp_dir, title)
+
+            # Create the new project directory and a 'templates' directory within it
+            os.makedirs(os.path.join(project_directory,
+                        'templates'), exist_ok=True)
+
+            # Print the created directory path
+            print(f"Project directory: {project_directory} has been created.")
+
+            # Create the 'main.py' for the new project
+            with open(os.path.join(project_directory, 'main.py'), 'w') as f:
+                f.write(f'''
+from flask import Flask, request, render_template
+import openai
+
+openai.api_key = '{open_id}'
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        n_values1 = []
+        for i in range(0, {variable_input_2}):
+            input_name1 = 'input_' + str(i)
+            value1 = request.form.get(input_name1)
+            n_values1.append(value1)
+
+        prompt = f"{prompt}"
+
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=500
+        )
+
+        return render_template('index.html', n_values={n_values}, variable_input_2={variable_input_2}, result=response.choices[0].text.strip())
+
+    return render_template('index.html', n_values={n_values}, variable_input_2={variable_input_2})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+''')
+
+            print("main.py file created.")
+
+            # Create the 'index.html' template for the new project
+            with open(os.path.join(project_directory, 'templates', 'index.html'), 'w') as f:
+                f.write(f'''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Input Form</title>
+    <style>
+        body {{
+            background-color: black;
+            color: white;
+            text-align: center;
+        }}
+        
+        h2, form, p {{
+            text-align: center;
+        }}
+    </style>
+</head>
+<body>
+    <h2>{title}</h2><br>
+    <form action="/" method="post">
+        <div id="input_fields"></div>
+        <input type="submit" value="Generate">
+    </form>
+    <h2>Result</h2>
+    <p>{{{{result}}}}</p>
+    <script>
+        // Generate input fields based on 'no of variables'
+
+        var inputFields = '';
+        var n_values = {n_values};
+        for (var i = 0; i < {variable_input_2}; i++) {{
+            inputFields += '<label for="n_values_' + i + '">' + n_values[i] + '</label><br>';
+            inputFields += '<input type="text" id="input_' + i + '" name="input_' + i + '"><br><br>';
+        }}
+        document.getElementById('input_fields').innerHTML = inputFields;
+    </script>
+</body>
+</html>
+''')
+
+            print("index.html file created.")
+
+            # Create the 'result.html' template for the new project
+            with open(os.path.join(project_directory, 'templates', 'result.html'), 'w') as f:
+                f.write(f'''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Result</title>
+    <style>
+        body {{
+            background-color: black;
+            color: white;
+            text-align: center;
+        }}
+        
+        h2, p {{
+            text-align: center;
+        }}
+    </style>
+</head>
+<body>
+    <h2>Result</h2>
+    <p>{{{{result}}}}</p>
+</body>
+</html>
+''')
+
+            print("result.html file created.")
+
+            if os.path.exists(project_directory):
+                print("Yes the project folder exists !!!!")
+                project_folder_name = title
+
+                download_folder(project_directory,
+                                project_folder_name)
+
+                shutil.rmtree(project_directory)
+
+                return f"Project {project_folder_name} created successfully!"
+
+        except Exception as e:
+            print("An error occurred while generating the project:", e)
+
+    return render_template('index.html')
+
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+
+def download_folder(project_directory, folder_name):
+
+    # Create a temporary ZIP file to store the folder contents
+    zip_path = f'{get_downloads_folder_path()}/{folder_name}.zip'
+
+    zip_folder(project_directory, zip_path)
+
+    # # Send the ZIP file as a downloadable response
+    return send_file(f'{zip_path}', as_attachment=True, download_name=f'{folder_name}.zip')
+
+
+def get_downloads_folder_path():
+    # Retrieve the user's home directory
+    home_dir = os.path.expanduser("~")
+
+    # Determine the downloads folder path based on the operating system
+    if os.name == 'posix':  # macOS or Linux
+        return os.path.join(home_dir, 'Downloads')
+    elif os.name == 'nt':  # Windows
+        return os.path.join(home_dir, 'Downloads')
+    else:
+        raise NotImplementedError("Unsupported operating system")
+
+
+def zip_folder(folder_path, zip_path):
+    with ZipFile(zip_path, 'w') as zip_file:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zip_file.write(file_path, arcname=os.path.relpath(
+                    file_path, folder_path))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
